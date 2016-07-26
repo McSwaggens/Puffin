4a565405e9cb2831b61685ca562c2b6e2b46d26a
McSwaggens <McSwaggens@protonmail.com>
2016-07-27 07:34:55 +1000

    Finished Point class

--- Puffin/PASM Definitions/Point.cs
+++ Puffin/PASM Definitions/Point.cs
@@ -4,8 +4,23 @@ namespace Puffin
 {
 	public class Point
 	{
+		public static int staticID = 0;
+		public bool inFunction = false;
+		public int id;
+		
 		public Point ()
 		{
+			id = staticID++;
+		}
+		
+		public Point (int id)
+		{
+			this.id = id;
+		}
+		
+		public string ToPASM ()
+		{
+			return (inFunction ? ":" : "") + id;
 		}
 	}
 }
