985f5f7ba52e604f58095254f9e305f0e82b0302
McSwaggens <McSwaggens@protonmail.com>
2016-07-30 09:24:21 +1000

    Finished variable class

--- Puffin/Definitions/Variable.cs
+++ Puffin/Definitions/Variable.cs
@@ -7,8 +7,36 @@ namespace Puffin
 		public Type type;
 		public string name;
 		public Register register;
-		public Variable ()
+		
+		public Variable (Type type, string name, bool inFunction)
 		{
+			this.type = type;
+			this.name = name;
+			this.register = new Register (inFunction);
+		}
+		
+		public Variable (Type type, string name, Register register)
+		{
+			this.type = type;
+			this.name = name;
+		}
+		
+		public bool IsBaseType ()
+		{
+			return type is PredefineType;
+		}
+		
+		public bool IsUserDefinedType()
+		{
+			return type is UserDefineType;
+		}
+		
+		public string ID
+		{
+			get
+			{
+				return register.ToPASM ();
+			}
 		}
 	}
 }
